33049954396d692d19917f9e624f42fd335ef86d
Your Name <you@example.com>
2026-03-25 21:35:06 +0530

    Fix: Added missing TelegramBot import

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const https = require('https');
 const { parseText, parseSummaryRange } = require('./parser');
